50f27753d2850aa2bc857165ea951087252ad0a8
jjz17 <jasonjzhang17@gmail.com>
2021-12-27 00:06:08 -0500

    Implemented basic functionality of moving cards between piles through command line arguments

--- game.py
+++ game.py
@@ -41,19 +41,54 @@ class FreeCellModel:
             # pile.cards.append(card)
             self.cascade_piles[count % num_piles].cards.append(card)
 
+    # def move(self, source, index, target):
+    #     pile_type = source[0]
+    #     pile_ind = source[1:]
+    #     if pile_type in ['C', 'O'] and isinstance(int(pile_ind), int):
+    #         pile_ind = int(pile_ind)
+    #         if pile_type == 'C':
+    #             if len(self.cascade_piles) >= pile_ind:
+    #
+    #         else:
+    #
+    #     else:
+    #         print('Invalid move')
+    def move(self, source, index, target):
+        # Validate inputs
+        source_pile_type = source[0]
+        source_pile_ind = int(source[1:]) - 1
+        index = int(index) - 1
+        target_pile_type = target[0]
+        target_pile_ind = int(target[1:]) - 1
+        if source_pile_type == 'C':
+            if target_pile_type == 'F':
+                self.foundation_piles[target_pile_ind].append(self.cascade_piles[source_pile_ind].pop(index))
+        else:
+            print('Failed')
+
+    def play(self):
+        print(self)
+        playing = True
+        while playing:
+            user_input = input('Make a move, enter q to quit\n')
+            if user_input == 'q':
+                playing = False
+            else:
+                print(f'You input {user_input}')
+                args = user_input.split()
+                if len(args) != 3:
+                    print('Invalid number of inputs')
+                    continue
+                # for arg in args:
+                #     print(arg)
+                self.move(args[0], args[1], args[2])
+                print(self)
+        print('Process complete')
+
 
 def main():
-    # playing = True
-    # while playing:
-    #     user_input = input('Make a move, enter q to quit\n')
-    #     if user_input == 'q':
-    #         playing = False
-    #     else:
-    #         print(f'You input {user_input}')
-    # print('Process complete')
-
     model = FreeCellModel(4, 4)
-    print(model)
+    model.play()
 
 
 if __name__ == "__main__":
--- pile.py
+++ pile.py
@@ -2,6 +2,12 @@ class Pile:
     def __init__(self, cards):
         self.cards = cards
 
+    def append(self, element):
+        self.cards.append(element)
+
+    def pop(self, index):
+        return self.cards.pop(index)
+
 
 class FoundationPile(Pile):
     pass
