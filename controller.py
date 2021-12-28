13f24d7e45f71ffd3730fb1c5059afca6f4756ee
jjz17 <jasonjzhang17@gmail.com>
2021-12-28 10:42:51 -0500

    Model constructor filters out invalid arguments and controller persists in requesting user input until a valid game is created

--- controller.py
+++ controller.py
@@ -8,11 +8,26 @@ class Controller:
     def play(self):
         print('Customize your FreeCell Solitaire game by specifying the number of cascade and open piles you would '
               'like.\n The default is 4 cascade and 4 open piles.\n To play the default game, hit enter at the prompt.')
-        num_cascade = int(input('# Cascade Piles: '))
-        num_open = int(input('# Open Piles: '))
-        if num_cascade != 4 or num_open != 4:
-            self.model = FreeCellModel(num_cascade, num_open)
+        started = False
+        while not started:
+            num_cascade = input('# Cascade Piles: ')
+            if num_cascade.split():
+                num_open = input('# Open Piles: ')
+                num_cascade = Controller.valid_integer_check(num_cascade)
+                num_open = Controller.valid_integer_check(num_open)
+                try:
+                    self.model = FreeCellModel(num_cascade, num_open)
+                    started = True
+                except ValueError as e:
+                    print(e)
         self.model.play()
+
+    @staticmethod
+    def valid_integer_check(integer):
+        try:
+            return int(integer)
+        except ValueError:
+            raise TypeError('Not an integer')
 
 def main():
     controller = Controller()
